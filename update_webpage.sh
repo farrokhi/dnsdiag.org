ed5fccb413cbb2f2197d4a2c08fe737a5ff92e9b
Babak Farrokhi <babak@farrokhi.net>
2016-05-07 16:38:45 +0430

    basic structure

---
+++ update_webpage.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ ! -d dnsdiag ]; then
+  git clone https://github.com/farrokhi/dnsdiag.git
+fi
+
+cd dnsdiag
+git pull
+cd -
+cat << HEADER > index.html
+<!DOCTYPE html>
+<html>
+<title>DNSDiag</title>
+
+<xmp theme="cerulean" style="display:none;">
+HEADER
+cat dnsdiag/README.md >> index.html
+cat << FOOTER >> index.html
+</xmp>
+
+<script src="/v/0.2/strapdown.js"></script>
+</html>
+FOOTER
+
